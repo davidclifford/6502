c6125713ada22e42024394c17f10989978fbdc8e
David Clifford <david.clifford.142@gmail.com>
2024-04-25 14:58:46 +1000

    32x22 Mandelbrot using Colour

--- Examples/mandelbrot.s
+++ Examples/mandelbrot.s
@@ -9,12 +9,53 @@
 loopy:
     ldx #0
 loopx:
+    lda #27
+    jsr IO_ECHO
+    lda #'['
+    jsr IO_ECHO
+
     jsr mand_get
-    adc #'!'
+    sta $80
+    bbs3 $80,lighter
+;    and #8
+;    bne lighter
+
+    lda #'4'
+    jsr IO_ECHO
+    lda $80
+    bra square
+lighter:
+    lda #'1'
+    jsr IO_ECHO
+    lda #'0'
+    jsr IO_ECHO
+    lda $80
+    inc
+square:
+    and #7
+    clc
+    adc #'0'
+    jsr IO_ECHO
+
+    lda #'m'
+    jsr IO_ECHO
+    lda #' '
+    jsr IO_ECHO
+    lda #' '
     jsr IO_ECHO
     inx
     cpx #MAND_WIDTH
     bne loopx
+    lda #27
+    jsr IO_ECHO
+    lda #'['
+    jsr IO_ECHO
+    lda #'4'
+    jsr IO_ECHO
+    lda #'0'
+    jsr IO_ECHO
+    lda #'m'
+    jsr IO_ECHO
     lda #10
     jsr IO_ECHO
     iny
@@ -24,8 +65,5 @@ loopx:
     jsr IO_ECHO
     jmp $c100
 
-char_codes : .string ' .:-=x+*&$#X%8@ZZZZZZ'
-
     .include mandel.s
     .include io.s
-
